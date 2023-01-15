1b4a32a162e5e355a9895a3ceef14cff37fcb8e9
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2023-01-15 15:29:31 +0100

    Update +page.svelte

--- app/src/routes/other_page/+page.svelte
+++ app/src/routes/other_page/+page.svelte
@@ -1,5 +1,5 @@
 <article class="prose md:prose-lg lg:prose-xl">
-  <h1>Another page!</h1>
+  <h2>Another page!</h2>
   <p>
     This is another page. Check the file under <code>routes/other_page/+page.svelte</code>
   </p>
@@ -9,3 +9,9 @@
   </p>
   <a target="_blank" rel="noopener noreferrer" href="https://tailwindcss.com/docs/typography-plugin">Tailwind Typography docs</a>
 </article>
+
+<style>
+    article {
+        padding: 1rem;
+    }
+</style>
