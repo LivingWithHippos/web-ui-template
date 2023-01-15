2109973b8a30a535781b2d969bb3e0fb7f9ea8a1
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2023-01-15 11:05:38 +0100

    added app skeleton

---
+++ app/README.md
@@ -0,0 +1,7 @@
+# INTERFACE
+
+This project will contain all the new web interfaces.
+
+## Installation
+
+Install [Node.js](https://nodejs.org/en/), go with a terminal inside the folder with `package.json` and run `npm install`
---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    // Import our global CSS
+    import "$lib/global.css";
+</script>
+
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a href="/" class="btn btn-ghost normal-case text-l">Change me</a>
+    </div>
+    <div class="flex-none">
+        <ul class="menu menu-horizontal p-0">
+            <li><a href="/">Item #1</a></li>
+            <li><a href="/item_2">Item #2</a></li>
+        </ul>
+    </div>
+</div>
+
+<slot></slot>
