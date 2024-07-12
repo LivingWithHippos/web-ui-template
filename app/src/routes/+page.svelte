8dcb12726ac92349968e063412e4fb698469a058
LivingWithHippos <livingwithhippos@users.noreply.github.com>
2024-07-12 12:15:24 +0200

    replaced code with sveltekit v2 skeleton

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,9 +1,2 @@
-<div class="hero min-h-screen bg-base-200">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      <h1 class="text-5xl font-bold">¡Hola, mundo!</h1>
-      <p class="py-6">This is a hero banner. Check out all the DaisyUI components.</p>
-      <button class="btn btn-primary"><a target="_blank" rel="noopener noreferrer" href="https://daisyui.com/components/">Get Started</a></button>
-    </div>
-  </div>
-</div>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,8 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-// https://github.com/sveltejs/kit/tree/master/packages/adapter-static
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,15 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter: adapter()
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		})
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
